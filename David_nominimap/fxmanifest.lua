fc353afff8dbc260a1c17e0de77bcc6be76c554d
DaaViDeXe <86785596+DaaViDeXe@users.noreply.github.com>
2021-08-11 21:57:57 +0200

    Add files via upload

---
+++ David_nominimap/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant' --2021
+games { 'gta5' }
+
+author '! David.eXe™#0001'
+description 'Quita el MiniMapa para tener una mejor visión en el juego'
+version '1.0.1' -- 1.0.(1) = Optimizado
+
+
+client_scripts {
+    'client/main.lua',
+
+}
